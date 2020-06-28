4c4954d26b91740d5b06c315618c5a736d6cfbaf
joinichi8 <joinichi8@gmail.com>
2020-06-28 15:59:04 +0300

    added bottle collector job

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 /* main.js */
+var bottle = 0;
+
+function bottleClick(number) {
+  bottle = bottle + number;
+  document.getElementById('bottle').innerHTML = bottle;
+}
+
+var money = 0;
+
+function sellbottle() {
+  if(bottle > 0)
+  {
+    bottle = bottle - 1;
+    document.getElementById('bottle').innerHTML = bottle;
+    document.getElementById('money').innerHTML
+    = Math.round(money += 0.4 * 100) / 100; 
+  }
+}
