2465e902a6c8bba49feb1693e3afd3527a2df60c
joinichi8 <joinichi8@gmail.com>
2020-07-11 09:22:47 +0300

    started with bussiness.html

--- main.js
+++ main.js
@@ -14,6 +14,24 @@ function sellbottle() {
     bottle = bottle - 1;
     document.getElementById('bottle').innerHTML = bottle;
     document.getElementById('money').innerHTML
-    = Math.round(money += 0.4 * 100) / 100; 
+    = Math.round(money += 0.4 * 100) / 100;
   }
 }
+
+// bussiness.html functions
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
